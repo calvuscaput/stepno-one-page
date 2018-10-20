8b3bac538ba6b8206a0301c85ea3fd846540998e
CalvusCaput <CalvusCaput@yandex.ru>
2018-10-20 10:29:40 +0300

    Added sliders

---
+++ dist/js/main.js
@@ -0,0 +1,54 @@
+$('.owl-carousel__1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 1,
+})
+
+$('.owl-carousel__2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 1,
+})
+var nextBtn = $(".car__right");
+var prevBtn = $(".car__left");
+//Третья карусель
+var rdCarousel = $('.owl-carousel__3')
+rdCarousel.owlCarousel({
+    onInitialized: slideInit,
+    onChanged: slidePosition,
+    loop:false,
+    margin:10,
+    nav:false,
+    items: 1,
+    dots: false,
+})
+
+function slideInit(e){
+    rdCarousel.slideCount = e.item.count;
+}
+function slidePosition(e) {
+    rdCarousel.slidePos = e.item.index+1
+    console.log(rdCarousel.slidePos, rdCarousel.slideCount);
+    
+    if (rdCarousel.slidePos == rdCarousel.slideCount) {
+        nextBtn.addClass('disabled');
+    } else {
+        nextBtn.removeClass('disabled');
+    }
+
+    if (rdCarousel.slidePos == 1) {
+        prevBtn.addClass('disabled');
+    } else {
+        prevBtn.removeClass('disabled');
+    }
+
+}
+//Работа с копками навигации
+nextBtn.click(function(){
+    rdCarousel.trigger("next.owl.carousel");
+});
+prevBtn.click(function(){
+    rdCarousel.trigger("prev.owl.carousel");
+});
